98025c89224aa8cf23c782d1e4c5af142607ddc5
Wyatt Arent <hello@wyatt.engineer>
2017-09-05 21:04:22 -0700

    Update

--- helpers/action_bundle_eval_test.go
+++ helpers/action_bundle_eval_test.go
@@ -14,9 +14,9 @@ func TestActionBundleEval(t *testing.T) {
 	AAS := models.AumActionSet{}
 	AAS.PlaySounds = make([]models.ARAPlaySound, 2)
 	AAS.PlaySounds[0].SoundType = models.ARAPlaySoundTypeText
-	AAS.PlaySounds[0].Value = "Hello world"
+	AAS.PlaySounds[0].Val = "Hello world"
 	AAS.PlaySounds[1].SoundType = models.ARAPlaySoundTypeAudio
-	AAS.PlaySounds[1].Value, _ = url.Parse("https://upload.wikimedia.org/wikipedia/commons/b/bb/Test_ogg_mp3_48kbps.wav")
+	AAS.PlaySounds[1].Val, _ = url.Parse("https://upload.wikimedia.org/wikipedia/commons/b/bb/Test_ogg_mp3_48kbps.wav")
 
 	runtimeState := models.AumMutableRuntimeState{
 		State:      map[string]interface{}{},
