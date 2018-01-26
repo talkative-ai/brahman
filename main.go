97ad9f29312e1639221d7627395ce507e8e93bf8
Wyatt Arent <hello@wyatt.engineer>
2018-01-25 22:49:05 -0800

    Connect to instances when server starts

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/artificial-universe-maker/brahman/routes"
+	"github.com/artificial-universe-maker/core/db"
+	"github.com/artificial-universe-maker/core/redis"
 	"github.com/artificial-universe-maker/core/router"
 
 	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	// Initialize database and redis connections
+	// TODO: Make it a bit clearer that this is happening, and more maintainable
+	err := db.InitializeDB()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Instance.Close()
+
+	_, err = redis.ConnectRedis()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer redis.Instance.Close()
 
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
